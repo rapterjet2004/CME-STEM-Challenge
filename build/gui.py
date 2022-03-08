379909ce61c187d9e938cf45de1b78c882040675
rapterjet2004 <juliuslinus1@gmail.com>
2022-03-08 10:26:52 -0600

    test change

--- build/gui.py
+++ build/gui.py
@@ -6,6 +6,7 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
 
 
+
 def plot_btmain():
     """
     Embeds the Backtrader plot from btmain into the tKinter window. Master canvas
